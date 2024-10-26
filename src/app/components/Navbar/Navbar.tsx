2925d0b02fa8232171bf5724d04385219186d219
MuhammadRaedSiddiquie <bhairaed636@gmail.com>
2024-10-26 07:52:35 +0500

    errors fix

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Style from './navbar.module.css'
+
 
 function Navbar() {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import Head from "next/head";
-import Link from "next/link";
 import Navbar from "./components/Navbar/Navbar"
 import Hero from "./components/Hero/Hero"
 import Footer from "./components/Footer/Footer";
